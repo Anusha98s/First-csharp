191c2ce9db3aca7e7020f1d765e4b7c053d7bdfe
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-14 10:23:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace First_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("What is your fav snack\n");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("How manyPlates you can have it at a time?\n");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(name);
+            Console.WriteLine(count);
+                            if(count >= 2)
+            {
+                Console.WriteLine("Too much of snacking is not a healthy sign....!!");
+            }
+        }
+    }
+}
